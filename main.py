48eb7482b96fec664256d053f5bca0a17d7fc53b
Saleh <sabasipyh@gmail.com>
2023-08-09 23:16:37 +0330

    genetic

--- main.py
+++ main.py
@@ -1,35 +1,79 @@
 import random
 import matplotlib.pyplot as plt
 
-locations = [(2.5, 9, 1),  # Starbucks
-             (3, 3, 1),
-             (8.5, 16, 1),
-             (7, 5, 1),
-             (1, 2, 2),  # Metro
-             (1.5, 11, 2),
-             (3.5, 5, 2),
-             (4, 19, 2),
-             (8, 1, 2),
-             (12, 10, 2),
-             (2, 4, 3),  # Gas
-             (5, 15, 3),
-             (6, 7, 3),
-             (10, 8, 3), ]
+points = [
+    (2.5, 9),  # Starbucks
+    (3, 3),
+    (8.5, 16),
+    (7, 5),
+    (1, 2),  # Metro
+    (1.5, 11),
+    (3.5, 5),
+    (4, 19),
+    (8, 1),
+    (12, 10),
+    (2, 4),  # Gas
+    (5, 15),
+    (6, 7),
+    (10, 8),
+]
+
+weights = [1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3]
 
 
-def distance(p1, p2):
+def manhattan_distance(p1, p2):
     return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
 
-def fitness(solution):
-    total_dist = 0
+def calculate_weighted_avg(individual):
+    total_distance = 0
     total_weight = 0
 
-    for p in solution:
-        min_dist = float('inf')
-        for loc in locations:
-            min_dist = min(min_dist, distance(p, loc))
-            total_weight += loc[2]
-        total_dist += min_dist
+    for index, p in enumerate(points):
+        distance = manhattan_distance(individual, p)
+        total_distance += distance * weights[index]
+        total_weight += weights[p[0]]
 
-    return total_dist / total_weight
+    return total_distance / total_weight
+
+
+population_size = 100
+mutation_chance = 0.01
+num_generations = 100
+
+population = []
+for i in range(population_size):
+    x = random.randint(0, 12)
+    y = random.randint(0, 20)
+    population.append((x, y))
+
+for generation in range(num_generations):
+    fitness_values = []
+    for individual in population:
+        fitness = calculate_weighted_avg(individual)
+        fitness_values.append(fitness)
+
+    parents = []
+    for i in range(population_size // 2):
+        parent1 = random.choices(population, weights=fitness_values)[0]
+        parent2 = random.choices(population, weights=fitness_values)[0]
+        parents.append((parent1, parent2))
+
+    children = []
+    for parent1, parent2 in parents:
+        child = (0, 0)
+        for i in range(len(parent1)):
+            if random.random() < 0.5:
+                child[i] = parent1[i]
+            else:
+                child[i] = parent2[i]
+
+        if random.random() < mutation_chance:
+            child = (random.randint(0, 12), random.randint(0, 20))
+
+        children.append(child)
+
+    population = children
+
+best = min(population, key=calculate_weighted_avg)
+print(best)
