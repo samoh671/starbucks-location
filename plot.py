86135e5ab2919907dbcb3041d7647e596637b04f
Sana Mohammadi <sana.mohammadi671@gmail.com>
2023-08-15 14:21:24 +0330

    changed plot

--- plot.py
+++ plot.py
@@ -1,15 +1,19 @@
 import matplotlib.pyplot as plt
 
+color_map = {
+    1: 'b',
+    2: 'g',
+    3: 'r'
+}
 
-def plot(init_locs, best_locs):
+
+def plot(init_locs: dict, best_locs):
     plt.figure(figsize=(6, 6))
 
     plt.axis([0, 12, 0, 20])
 
-    # plt.scatter([loc[0] for loc in init_locs], [loc[1] for loc in init_locs], c='r')
-    plt.scatter([loc[0] for loc in init_locs[:3]], [loc[1] for loc in init_locs[:3]], c='g')
-    plt.scatter([loc[0] for loc in init_locs[4:9]], [loc[1] for loc in init_locs[4:9]], c='b')
-    plt.scatter([loc[0] for loc in init_locs[10:]], [loc[1] for loc in init_locs[10:]], c='r')
+    for loc, weight in init_locs.items():
+        plt.scatter(loc[0], loc[1], c=color_map[weight])
 
     plt.scatter([loc[0] for loc in best_locs], [loc[1] for loc in best_locs], c='c')
 
